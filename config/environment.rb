da623a452f44af04d751fd4179ba61a80e0fbfd7
Trevor Turk <trevorturk@yahoo.com>
2009-07-26 16:36:27 -0500

    add right_aws gems for s3 compat

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ Rails::Initializer.run do |config|
   config.frameworks -= [:active_resource, :action_mailer ]
   config.time_zone = 'UTC'
   config.gem 'thoughtbot-paperclip', :version => '2.3.0', :lib => 'paperclip', :source => 'http://gems.github.com'
+  config.gem 'right_aws', :version => '1.9.0'
+  config.gem 'right_http_connection', :version => '1.2.4'
   CONFIG['s3_access_id'] && CONFIG['s3_secret_key'] && CONFIG['s3_bucket_name'] ? CONFIG['s3'] = true : CONFIG['s3'] = false
   config.action_controller.session = {
     :key => CONFIG['session_key'],
