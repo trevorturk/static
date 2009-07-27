0578cf97425694653e18fc3c06ac10c48091ac3a
Trevor Turk <trevorturk@yahoo.com>
2009-07-26 22:38:26 -0500

    clean up styles, move edit_page_path so admin can just add /edit after page url to edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'pages', :action => 'home'
-  map.resources :pages, :except => [:index]
+  map.resources :pages, :except => [:index, :edit]
   map.resources :settings, :except => [:index, :show]
   map.resources :uploads, :only => [:new, :create, :destroy]
   map.admin '/admin', :controller => 'admin', :action => 'index'
+  map.edit_page '/:id/edit', :controller => 'pages', :action => 'edit'
   map.slug '/:id', :controller => 'pages', :action => 'show'
 end
