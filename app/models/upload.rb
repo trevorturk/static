733d994f258b751b1db9bd675d5dfc06bda471d0
Trevor Turk <trevorturk@yahoo.com>
2009-07-30 21:21:09 -0500

    small cleanup for s3 uploads

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -4,34 +4,36 @@ class Upload < ActiveRecord::Base
   attr_accessible :attachment, :attachment_url, :attachment_remote_url
   attr_accessor :attachment_url
   
+  STYLES = { :square => '75x75#', :thumb => '100x100>', :small => '240x240>', :medium => '500x500>', :large => '1024x1024>' }
+  
   if CONFIG['s3']
     has_attached_file :attachment, 
                       :storage => :s3, 
-                      :path => ":basename:normalized_style.:extension", 
+                      :path => ":id/:basename:normalized_style.:extension", 
                       :default_style => :original,
                       :bucket => CONFIG['s3_bucket_name'],
                       :s3_credentials => { :access_key_id => CONFIG['s3_access_id'], :secret_access_key => CONFIG['s3_secret_key'] },
                       :s3_headers => { 'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate },
-                      :styles => { :square => '75x75#', :thumb => '100x100>', :small => '240x240>', :medium => '500x500>', :large => '1024x1024>' }
+                      :styles => STYLES
   else
     has_attached_file :attachment, 
                       :storage => :filesystem, 
-                      :url => "/uploads/:basename:normalized_style.:extension",
+                      :url => "/uploads/:id/:basename:normalized_style.:extension",
                       :default_style => :original,
-                      :styles => { :square => '75x75#', :thumb => '100x100>', :small => '240x240>', :medium => '500x500>', :large => '1024x1024>' }
+                      :styles => STYLES
   end
   
   Paperclip.interpolates :normalized_style do |attachment, style|
     "_#{style}" if attachment.instance.image? && style != :original
   end
-  
+    
   before_validation :download_remote_file, :if => :attachment_url_provided?
   
   validates_presence_of :attachment_file_name
   validates_presence_of :attachment_remote_url, :if => :attachment_url_provided?, :message => 'is invalid or inaccessible'
   validates_uniqueness_of :attachment_file_name
   
-  before_post_process :image?
+  before_post_process :image? # bypass image processing if not an image
   
   def image?
     !(attachment_content_type =~ /^image.*/).nil?
