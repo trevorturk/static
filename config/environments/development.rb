7624dd721f1a1ccdcf9289da832e6812ade87e82
Trevor Turk <trevorturk@gmail.com>
2010-02-10 20:07:40 -0600

    use homebrew dir for imagemagick

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,4 +6,4 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = false
 config.logger = Logger.new(config.log_path, 2, 20.megabytes) # Automatically rotate log files
-Paperclip.options[:command_path] = '/opt/local/bin/' # for OS X with passenger
+Paperclip.options[:command_path] = '/usr/local/bin/' # for imagemagick with homebrew
