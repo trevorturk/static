56caa531a05468d7336d41151f0bbb9f37ada99e
Trevor Turk <trevorturk@gmail.com>
2010-02-22 12:27:24 -0600

    more bundler 0.9

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,8 @@ Rails::Initializer.run do |config|
   require 'digest/md5'
   require 'yaml'
   CONFIG = (YAML.load_file('config/config.yml')[RAILS_ENV] rescue {}).merge(ENV)
-  CONFIG['s3'] = true if CONFIG['s3_access_id'] && CONFIG['s3_secret_key'] && CONFIG['s3_bucket_name']  
+  CONFIG['s3'] = true if CONFIG['s3_access_id'] && CONFIG['s3_secret_key'] && CONFIG['s3_bucket_name']
   config.frameworks -= [:active_resource, :action_mailer ]
   config.time_zone = 'UTC'
-  config.action_controller.session = {
-    :key => CONFIG['session_key'],
-    :secret => CONFIG['session_secret']
-  }
+  config.action_controller.session = { :key => CONFIG['session_key'], :secret => CONFIG['session_secret'] }
 end
