da1d9cff35c3c4a0631444350e8ef7d0a0df667c
Trevor Turk <trevorturk@gmail.com>
2010-02-22 12:25:02 -0600

    bundler 0.9

--- config/boot.rb
+++ config/boot.rb
@@ -106,5 +106,22 @@ module Rails
   end
 end
 
-# All that for this:
+class Rails::Boot
+  def run
+    load_initializer
+    extend_environment
+    Rails::Initializer.run(:set_load_path)
+  end
+
+  def extend_environment
+    Rails::Initializer.class_eval do
+      old_load = instance_method(:load_environment)
+      define_method(:load_environment) do
+        Bundler.require :default, Rails.env
+        old_load.bind(self).call
+      end
+    end
+  end
+end
+
 Rails.boot!
