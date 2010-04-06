14626dbab7f003a57d832ec8e6a7676ac6ff4995
Trevor Turk <trevorturk@gmail.com>
2010-04-06 14:43:49 -0500

    switching to new paperclip with aws/s3

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,6 @@ Rails::Initializer.run do |config|
   config.action_controller.session = {:key => "static", :secret => ActiveSupport::SecureRandom.hex(32)}
   config.frameworks -= [:active_resource, :action_mailer]
   config.time_zone = 'UTC'
-  config.gem "paperclip", :version => "2.3.0"
-  config.gem "right_aws", :version => "1.9.0"
-  config.gem "right_http_connection", :version => "1.2.4"
+  config.gem "paperclip"
+  config.gem "aws-s3", :lib => "aws/s3"
 end
